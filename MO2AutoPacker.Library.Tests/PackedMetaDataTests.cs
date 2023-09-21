66ebc5db94c28b3e71cf8fc046e467e4e56ebc30
Cody Lyttle <codylyttle123@gmail.com>
2023-09-22 03:00:40 +1200

    Add PackedMetaData class.

---
+++ MO2AutoPacker.Library.Tests/PackedMetaDataTests.cs
@@ -0,0 +1,132 @@
+﻿using System.Globalization;
+using FluentAssertions;
+
+namespace MO2AutoPacker.Library.Tests;
+
+public class PackedMetaDataTests
+{
+    [Fact]
+    public void ToString_ShouldReturnFormattedString()
+    {
+        // Arrange
+        PackedMetaData testTarget = new("MyArchive", 1024, DateTime.Now, new[] {"modA", "modB", "modC"});
+
+        string expected = $"archive={testTarget.ArchiveName}\r\n" +
+                          $"size={testTarget.Size}\r\n" +
+                          $"time={testTarget.PackedAt.ToString(DateTimeFormatInfo.InvariantInfo)}\r\n" +
+                          $"{testTarget.Mods[0]}\r\n" +
+                          $"{testTarget.Mods[1]}\r\n" +
+                          $"{testTarget.Mods[2]}\r\n";
+
+        // Act
+        var actual = testTarget.ToString();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void ReadFromString_ShouldReturnEquivalentObject_WhenValidString()
+    {
+        // Arrange
+        PackedMetaData expected = new("MyArchive", 1024, DateTime.Now, new[] {"modA", "modB", "modC"});
+        string input = $"archive={expected.ArchiveName}\r\n" +
+                       $"size={expected.Size}\r\n" +
+                       $"time={expected.PackedAt.ToString(DateTimeFormatInfo.InvariantInfo)}\r\n" +
+                       $"{expected.Mods[0]}\r\n" +
+                       $"{expected.Mods[1]}\r\n" +
+                       $"{expected.Mods[2]}\r\n";
+
+        // Act
+        PackedMetaData actual = PackedMetaData.ReadFromString(input);
+
+        // Assert
+        Assert.Equal(expected.ArchiveName, actual.ArchiveName);
+        Assert.Equal(expected.Size, actual.Size);
+        Assert.Equal(expected.PackedAt.ToLongTimeString(), actual.PackedAt.ToLongTimeString());
+        Assert.Equivalent(expected.Mods, actual.Mods);
+    }
+
+    [Fact]
+    public void ReadFromString_ShouldThrowFormatException_WhenMissingHeaders()
+    {
+        const int headerCount = 3;
+
+        (string Key, string Value)[] headersSource =
+        {
+            new("archive=", "MyArchive"),
+            new("size=", "1024"),
+            new("time=", "09/22/2023 02:04:31"),
+        };
+
+        var headers = new (string Key, string Value)[headerCount];
+        headersSource.CopyTo(headers, 0);
+
+        for (var i = 0; i < headerCount; i++)
+        {
+            // Read with temporarily removed header key.
+            headers[i].Key = string.Empty;
+            Action act = () => PackedMetaData.ReadFromString(BuildInput(headers));
+            act.Should().Throw<FormatException>().WithMessage($"Missing header '{headersSource[i].Key}'");
+            headers[i].Key = headersSource[i].Key;
+
+            // Read with temporarily removed header value.
+            headers[i].Value = string.Empty;
+            act = () => PackedMetaData.ReadFromString(BuildInput(headers));
+            act.Should().Throw<FormatException>().WithMessage($"Missing value for header '{headersSource[i].Key}'");
+            headers[i].Value = headersSource[i].Value;
+        }
+
+        string BuildInput(IReadOnlyList<(string Key, string Value)> headerPairs)
+        {
+            return $"{headerPairs[0].Key}{headerPairs[0].Value}\r\n" +
+                   $"{headerPairs[1].Key}{headerPairs[1].Value}\r\n" +
+                   $"{headerPairs[2].Key}{headerPairs[2].Value}\r\n" +
+                   $"modA\r\n" +
+                   $"modB\r\n" +
+                   $"modC\r\n";
+        }
+    }
+
+    [Fact]
+    public void ReadFromString_ShouldThrowFormatException_WhenBadSizeFormat()
+    {
+        // Arrange
+        const string input = "archive=MyArchive\r\n" +
+                             "size=NotAnInt32\r\n" + 
+                             "time=09/22/2023 02:04:31\r\n";
+        
+        // Act/Assert
+        Action act = () => PackedMetaData.ReadFromString(input);
+        act.Should().Throw<FormatException>()
+            .WithMessage("Expected Int32 value for header 'size='");
+    }
+
+    [Fact]
+    public void ReadFromString_ShouldThrowFormatException_WhenBadTimeFormat()
+    {
+        // Arrange
+        const string input = "archive=MyArchive\r\n" +
+                             "size=1024\r\n" + 
+                             "time=NotADateTime\r\n";
+        
+        // Act/Assert
+        Action act = () => PackedMetaData.ReadFromString(input);
+        act.Should().Throw<FormatException>()
+            .WithMessage("Expected DateTime value for header 'time='");
+    }
+
+    [Fact]
+    public void ReadFromString_ShouldThrowFormatException_WhenNoModNames()
+    {
+        // Arrange
+        const string input = "archive=MyArchive\r\n" +
+                             "size=1024\r\n" + 
+                             "time=09/22/2023 02:04:31\r\n";
+        
+        // Act/Assert
+        Action act = () => PackedMetaData.ReadFromString(input);
+        act.Should().Throw<FormatException>()
+            .WithMessage("Missing packed mod names");
+    }
+}
---
+++ MO2AutoPacker.Library/PackedMetaData.cs
@@ -0,0 +1,93 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace MO2AutoPacker.Library;
+
+// TODO: Further define packed mod info, eg.
+//       ModName    // Contains entire mod.
+//       MyMod**    // Contains beginning of partial mod
+//       **MyMod**  // Contains mid-point of partial mod.
+//       **MyMod    // Contains end of partial mod.
+public class PackedMetaData
+{
+    public const string ArchiveHeader = "archive=";
+    public const string SizeHeader = "size=";
+    public const string TimeHeader = "time=";
+
+    public string ArchiveName { get; }
+    public int Size { get; }
+    public DateTime PackedAt { get; }
+    public string[] Mods { get; }
+
+    public PackedMetaData(string archiveName, int size, DateTime packedAt, IEnumerable<string> mods)
+    {
+        ArchiveName = archiveName;
+        Size = size;
+        PackedAt = packedAt;
+        Mods = mods.ToArray();
+    }
+
+    public static PackedMetaData ReadFromString(string content)
+    {
+        using StringReader reader = new(content);
+
+        string archive = ReadHeader(ArchiveHeader, reader.ReadLine());
+
+        int size;
+        if (!int.TryParse(ReadHeader(SizeHeader, reader.ReadLine()), out size))
+        {
+            throw new FormatException($"Expected Int32 value for header '{SizeHeader}'");
+        }
+
+        DateTime time;
+        if (!DateTime.TryParse(ReadHeader(TimeHeader, reader.ReadLine()), DateTimeFormatInfo.InvariantInfo, out time))
+        {
+            throw new FormatException($"Expected DateTime value for header '{TimeHeader}'");
+        }
+
+        List<string> mods = new();
+        string? nextMod = reader.ReadLine();
+        while (nextMod != null)
+        {
+            nextMod = nextMod.Trim();
+            if (nextMod != string.Empty)
+            {
+                mods.Add(nextMod);
+            }
+
+            nextMod = reader.ReadLine();
+        }
+
+        if (mods.Count == 0)
+        {
+            throw new FormatException("Missing packed mod names");
+        }
+
+        return new PackedMetaData(archive, size, time, mods);
+    }
+
+    private static string ReadHeader(string headerName, string? line)
+    {
+        if(line == null || !line.Contains(headerName))
+            throw new FormatException($"Missing header '{headerName}'");
+
+        string[] split = line.Trim().Split(headerName, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        if (split.Length == 0)
+            throw new FormatException($"Missing value for header '{headerName}'");
+        
+        return split[0].Trim();
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new();
+
+        sb.Append(ArchiveHeader).AppendLine(ArchiveName);
+        sb.Append(SizeHeader).AppendLine(Size.ToString());
+        sb.Append(TimeHeader).AppendLine(PackedAt.ToString(DateTimeFormatInfo.InvariantInfo));
+        foreach (string mod in Mods)
+            sb.AppendLine(mod);
+
+        return sb.ToString();
+    }
+}
