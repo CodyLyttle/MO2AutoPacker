34796fe87785084c69b50909d52841237c13276d
Cody Lyttle <codylyttle123@gmail.com>
2023-09-25 18:46:13 +1300

    Add missing class.

---
+++ MO2AutoPacker.Library.Tests/Helpers/BannerMessageReceiver.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using MO2AutoPacker.Library.Messages;
+
+namespace MO2AutoPacker.Library.Tests.Helpers;
+
+internal sealed class BannerMessageReceiver : IRecipient<BannerMessage>
+{
+    public BannerMessageReceiver(IMessenger messenger)
+    {
+        messenger.Register(this);
+    }
+
+    public Queue<BannerMessage> Messages { get; } = new();
+
+    public void Receive(BannerMessage message) => Messages.Enqueue(message);
+}
