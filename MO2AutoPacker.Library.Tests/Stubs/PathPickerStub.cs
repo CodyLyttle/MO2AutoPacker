0fdcfb56eb76243f56b788d2d1bd09c131c860ef
Cody Lyttle <codylyttle123@gmail.com>
2023-09-27 14:53:50 +1300

    Add PackPickerStub.

---
+++ MO2AutoPacker.Library.Tests/Stubs/PathPickerStub.cs
@@ -0,0 +1,17 @@
+﻿using MO2AutoPacker.Library.Services;
+
+namespace MO2AutoPacker.Library.Tests.Stubs;
+
+public class PathPickerStub : IPathPicker
+{
+    public DirectoryInfo? DirectoryToReturn { get; set; } = null;
+    public FileInfo? FileToReturn { get; set; } = null;
+
+    public DirectoryInfo? PickDirectory() => DirectoryToReturn;
+
+    public DirectoryInfo? PickDirectory(DirectoryInfo initialDirectory) => DirectoryToReturn;
+
+    public FileInfo? PickFile(params string[] extensionWhitelist) => FileToReturn;
+
+    public FileInfo? PickFile(DirectoryInfo initialDirectory, params string[] extensionWhitelist) => FileToReturn;
+}
